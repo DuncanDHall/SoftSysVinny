65a57a73ee64501bcc1a4bc6844e9a4edcc3673c
Ilya Besancon <ilya.besancon@students.olin.edu>
2019-04-13 16:08:32 -0400

    Resolving while loop errors

--- print_text.c
+++ print_text.c
@@ -10,49 +10,39 @@
 #include <unistd.h>
 
 // TODO get only lines in frame
-void read_lines_from_file(FILE *fp, char **lines_array, int start_line, int n_lines)
+void read_lines_from_file(FILE *fp, char ***lines_array_ptr, int start_line, int n_lines)
 {
-    rewind(fp); //rewinds pointer to top of file
-
-    char *array[n_lines];
-    int i; //array index
     char *line = NULL;
     size_t lsize;
-    int r;
-    ssize_t line_size;
     int line_counter = 0;
-    int writeflag;
-
-    /* Get the first line of the file. */
-    line_size = getline(&line, &lsize, fp);
+    //int index = 0;
+    rewind(fp); //rewinds pointer to top of file
 
     printf("reading thru file\n");
     /* Loop through until we are done with the file. */
-    while (line_size >= 0)
-    {
-      /* Increment our line count */
-      line_counter++;
+    /* Get the next line */
 
-      /* Show the line details */
-      printf("line[%06d]: chars=%06zd, buf size=%06zu, contents: %s", line_counter,
-          line_size, lsize, line);
-      //flag goes up: ready for reading
-      if(line_counter == start_line){
-        writeflag = 1;
-      }
-      //flag goes down, done writing
-      if(line_counter == (start_line + n_lines)){
-        writeflag = 0;
-      }
-      if(writeflag){
-        array[i] = line;
-        i++;
-      }
-      /* Get the next line */
-      line_size = getline(&line, &lsize, fp);
-      //printf("Line is \n", line);
+    while(getline(&line, &lsize, fp) != -1) {
+        //printf("Line is \n", line);
+        //debugging print statements
+        printf("Count is %i\n", line_counter);
+
+        /* Show the line details */
+        //printf("line[%06d]: chars=%06zd, buf size=%06zu, contents: %s", line_counter,
+        //  line_size, lsize, line);
+        //printf("Conditional: %d, %d\n", line_counter >= start_line, line_counter < start_line + n_lines);
+        if(line_counter >= start_line && line_counter < start_line + n_lines){
+            (*lines_array_ptr)[line_counter] = line; //character pointer
+            printf("WROTE LINE\n");
+            //printf("Line is: %s\n", line);
+        }
+        /* Increment our line count */
+        line_counter++;
     }
+    printf("BIG SAD BIG SAD\n");
+    free(line); //free line buffer
     /*
+
     while ((r = getline(&line, &lsize, fp)) != -1) {
         if(startline == linecounter){
             array[i] = line;
@@ -74,31 +64,31 @@ int count_digits(int n)
     return c;
 }
 
-//this function does tihs_____
-// void get_lines_to_print(FILE *fp, char **lines_array, int start_line, int n_lines)
-// {
-//     int gutter_size;
-//     char **raw_lines;
-//     char **lines;
-//
-//     // get info about window size
-//     // calculate gutter size
-//     gutter_size = 2 + count_digits(top_line + n_env_lines);
-//
-//     // get raw lines
-//     raw_lines = read_lines_from_file(fp, lines_array, start_line, end_line);
-//
-//     // trim lines
-//     // lines = malloc(sizeof(char *[n_env_lines]));
-//     // for (int )
-//
-//     // draw line numbers
-// }
+// TODO: this function does tihs_____
+/*
+void format_lines(FILE *fp, char **lines_array, int start_line, int n_lines)
+{
+    int gutter_size;
+    char **raw_lines;
 
-void print_lines(char **lines_array, int n_lines)
+    // get info about window size
+    // calculate gutter size
+    gutter_size = 2 + count_digits(top_line + n_env_lines);
+
+    // get raw lines
+    read_lines_from_file(fp, raw_lines, start_line, end_line);
+
+    // trim lines
+    // lines = malloc(sizeof(char *[n_env_lines]));
+    // for (int )
+
+    // draw line numbers
+}
+*/
+void print_lines(char ***lines_array, int n_lines)
 {
     for (int i = 0; i < n_lines; i++) {
-        printf("%s\n", lines_array[i]);
+        printf("%s\n", (*lines_array)[i]);
     }
 }
 
@@ -125,13 +115,14 @@ int main (int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
     n_env_lines = w.ws_row;
+    printf("Number of lines is: %i\n", n_env_lines);
     n_env_cols = w.ws_col;
 
     // allocate space for 5 pointers to strings
-    lines_array = (char**) malloc(n_env_lines*sizeof(char*));
-    read_lines_from_file(fp, lines_array, top_line, n_env_lines);
-
-    print_lines(lines_array, n_env_lines);
+    lines_array = (char**) malloc(n_env_lines*sizeof(char*)); // TODO: don't malloc
+    read_lines_from_file(fp, &lines_array, top_line, n_env_lines);
+    // printf("Array firstline: %s\n", lines_array[1]);
+    //print_lines(&lines_array, n_env_lines);
 
     // read_lines_from_file(fp,lines_array, 3,3); //reads lines
 
