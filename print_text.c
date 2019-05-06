f41ea1302b91049907e21ad24134ed8b6fac77aa
Ilya Besancon <ilya.besancon@students.olin.edu>
2019-05-06 17:48:04 -0400

    Pushing formatting changes

--- print_text.c
+++ print_text.c
@@ -11,7 +11,7 @@
 
 
 // Read all of the lines from file and put into an array
-// line in an array pointed to by lines_array_ptrd .
+// line in an array pointed to by lines_array_ptr .
 void read_lines_from_file(char *file_name, char ***lines_array_ptr)
 {
     char *line = NULL;
@@ -37,7 +37,7 @@ void read_lines_from_file(char *file_name, char ***lines_array_ptr)
     char **lines_array = malloc(sizeof(char *) * num_lines);
 
 
-    fseek(fp, 0, SEEK_END); //rewinds pointer to top of file
+    fseek(fp, 0, SEEK_SET); //rewinds pointer to top of file
 
     //printf("reading thru file\n");
     /* Loop through until we are done with the file. */
@@ -45,11 +45,11 @@ void read_lines_from_file(char *file_name, char ***lines_array_ptr)
 
     while(getline(&line, &lsize, fp) != -1) {
         lines_array[line_counter] = strdup(line);
-        // //printf("Line is \n", line);
+        //printf("Line is %s\n", line);
         //
         // /* Show the line details */
-        // //printf("line[%06d]: chars=%06zd, buf size=%06zu, contents: %s", line_counter,
-        // //  line_size, lsize, line);
+        // printf("line[%06d]: chars=%06zd, buf size=%06zu, contents: %s", line_counter,
+        //  line_size, lsize, line);
         // //printf("Conditional: %d, %d\n", line_counter >= start_line, line_counter < start_line + n_lines);
         // if(line_counter >= start_line && line_counter < start_line + n_lines){
         //     (*lines_array_ptr)[line_counter - start_line] = strdup(line); //character pointer
@@ -70,6 +70,8 @@ void read_lines_from_file(char *file_name, char ***lines_array_ptr)
     fclose(fp);
 
     *lines_array_ptr = lines_array;
+    // printf("Testing line from lines_array: %s\n", lines_array[3]);
+    // printf("Testing line: %s\n", (*lines_array_ptr)[3]);
 }
 
 //this function does this _____
@@ -87,42 +89,56 @@ int count_digits(int n)
 // number of lines and number of columns
 // formats them to fill the terminal display window
 // adds line numbers with a gutter
-// void get_formatted_lines(FILE *fp, char ***lines_array_ptr, int start_line, int n_lines, int n_cols)
-// {
-//     int gutter_size;
-//     char **raw_lines;
-//     char *line;
-//
-//     // get info about window size
-//     // calculate gutter size
-//     gutter_size = 2 + count_digits(start_line + n_lines);
-//
-//     // get raw lines
-//     read_lines_from_file(fp, &raw_lines, start_line, n_lines);
-//
-//     // write raw lines
-//     for (int i = 0; i < n_lines; i++) {
-//         line = malloc(n_cols * sizeof(char));
-//         memcpy(
-//             (void *)(&line + gutter_size),
-//             (void *) raw_lines[i],
-//             (strlen(raw_lines[i])+1-gutter_size) * sizeof(char)
-//         );
-//         // memcopy raw into line
-//     }
-//     *lines_array_ptr = raw_lines;
-//
-//     // trim lines
-//     // lines = malloc(sizeof(char *[n_env_lines]));
-//     // for (int )
-//
-//     // draw line numbers
-// }
+/*
+void get_formatted_lines(State *state, char ***lines_array_ptr, int n_lines, int n_cols)
+{
+    int gutter_size;
+    char **raw_lines;
+    char *line;
 
-void print_lines(char ***lines_array_ptr, int n_lines)
+    // get info about window size
+    // calculate gutter size
+    gutter_size = 2 + count_digits(start_line + n_lines);
+
+    // get raw lines
+    //read_lines_from_file(fp, &raw_lines, start_line, n_lines);
+
+    int line_length = strlen(raw_lines[i])+1;
+
+    char **formatted_line_ptrs = malloc(sizeof(char*)*n_lines);
+    // write raw lines
+    for (int i = 0; i < n_lines; i++) {
+        line = malloc(n_cols * sizeof(char));
+        *formatted_line_ptrs[i] = line;
+        // memcopy raw into line (destination, source, number of bytes)
+        if(line_length + gutter_size < n_cols){
+            memcpy(
+                (void *)(&line + gutter_size),
+                (void *) raw_lines[i],
+                (line_length) * sizeof(char)
+            );
+        }
+    }
+    *lines_array_ptr = raw_lines;
+
+    // trim lines
+    // lines = malloc(sizeof(char *[n_env_lines]));
+    // for (int )
+
+    // draw line numbers
+}
+*/
+
+void print_lines(char **lines_array_ptr, int n_lines)
 {
     for (int i = 0; i < n_lines; i++) {
-        printf("%s", (*lines_array_ptr)[i]);
+        // if(i<10){
+        //     printf("0%d %s", i, lines_array_ptr[i]);
+        // }
+        // else{
+        //     printf("%d %s", i, lines_array_ptr[i]);
+        // }
+        printf("%s\n",lines_array_ptr[i]);
     }
 }
 
@@ -138,7 +154,7 @@ enum Mode {
 // define the state of our window
 typedef struct {
     char **lines; // array of lines from our file
-    int top_line;
+    int top_line; // the line index of first line to be displayed
     int window_height, window_width; // window dimensions
     int cursor_row, cursor_col; //referenced to lines not window
     enum Mode mode;
@@ -160,18 +176,65 @@ State *make_state(char *file_name) {
 
     return state;
 }
-    // for reference:
-/*
-    Matrix *matrix = malloc(sizeof(Matrix));
-    matrix->num_rows = num_rows;
-    matrix->num_cols = num_cols;
-    matrix->rows = malloc(num_rows * sizeof(double*));
-    for (int i=s; i<num_rows; i++) {
-        matrix->rows[i] = calloc(num_cols, sizeof(double));
-    }
-    return matrix;
-}
+
+
+/*   Prints out the current "state" object
+    -formatted lines
+    -printed to window
+    -prints cursor with color indicator
+    -prints Mode
 */
+void print_state(State *state, int num_columns, int num_rows){
+    //print u"\u001b[31mHelloWorld\u001b[0m";
+    printf("\033[1;31m");
+    printf("Running function: print_state\n");
+    printf("\033[0m");
+
+    // int n_lines
+    int gutter_size;
+    char **raw_lines;
+    char *line;
+    char spaces[5] = "   ";
+    char zero[3] = "0";
+    char numbers[3];
+    // calculate gutter size
+    gutter_size = 2 + count_digits(state->top_line + num_rows);
+    // printf("Gutter is %d\n", gutter_size);
+
+    char **formatted_line_ptrs = malloc(sizeof(char*) * num_rows);
+    // write raw lines
+
+    char strC[50];
+
+    for (int i = 0; i < num_rows; i++) {
+        line = malloc(num_columns * sizeof(char));
+        int line_length = strlen(state->lines[i]+1);
+        // printf("Line length is %d\n", line_length);
+        char *newline = (state->lines)[i];
+
+        // if less than 10
+        char result[50];
+        sprintf(result, "%d", i);
+        if(i < 10){
+            strncpy(strC, zero, 4);
+            strcat(strC, result);
+            strncpy(result, strC, 5);
+        }
+
+        strncpy(line, result, 3);
+        strncat(line, spaces, 4);
+        if(line_length < num_columns){
+            strncat(line, newline, line_length);
+        }
+        else{
+            strncat(line, newline, num_columns-gutter_size-1);
+        }
+        formatted_line_ptrs[i] = line;
+    }
+    print_lines(formatted_line_ptrs, 24);
+    // print_lines((state->lines), 24);
+}
+
 
 int main (int argc, char *argv[])
 {
@@ -199,7 +262,7 @@ int main (int argc, char *argv[])
         printf("Number of lines is: %i\n", n_env_lines);
 
         // print(state, width, height)
-        // print_state(state, n_env_cols, n_env_lines);
+        print_state(state, n_env_cols, n_env_lines);
 
         break;
     }
